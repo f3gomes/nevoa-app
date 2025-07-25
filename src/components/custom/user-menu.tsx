e42c04f961cb0b0395f0db4c48808871bb2f8633
f3gomes <fgomesdeluna@gmail.com>
2025-07-25 17:39:01 -0300

    feat: user menu

---
+++ src/components/custom/user-menu.tsx
@@ -0,0 +1,37 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { CircleUser } from "lucide-react";
+
+interface UserMenuProps {
+  userName: string;
+  handleLogout: () => void;
+}
+
+export default function UserMenu({ userName, handleLogout }: UserMenuProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="bg-pink-600 hover:bg-pink-700 text-white">
+          <CircleUser className="mr-1" />
+          {userName}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-48 bg-neutral-900 text-white border border-neutral-700 ml-24">
+        <DropdownMenuItem disabled>
+          Logado como <strong>{userName}</strong>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={handleLogout}
+          className="cursor-pointer text-red-500"
+        >
+          Sair
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
